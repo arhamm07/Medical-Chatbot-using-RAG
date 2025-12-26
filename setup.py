77161962d866fe2dadda3696f8a882c20aca4d5f
arham <arhamop937@gmail.com>
2025-12-26 17:35:53 +0500

    local package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name= 'Medica_chatbot',
+    version= '1.0.0',
+    author= 'Arham Rafique',
+    author_email= 'arhammrafique@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
