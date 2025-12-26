1c87f6e7e406ba42b2f6ca648befa5104ff71972
arham <arhamop937@gmail.com>
2025-12-26 17:25:53 +0500

    folder structure defined in template.sh file

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Directories
+
+mkdir -p src
+mkdir -p research
+
+
+# Files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/experiment.ipynb
+touch requirements.txt
