14eb8bb7016c99d64be19cda2c78fe5f3bf38ed6
arham <arhamop937@gmail.com>
2025-12-28 14:06:45 +0500

    code to to perform loading,chunking and create embeddings and store in pinecone index

---
+++ store_index.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files, filter_to_minimal_doc, text_splitter, download_embeddings_model
+from pinecone import Pinecone
+from pinecone import ServerlessSpec 
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+GROQ_API_KEY=os.environ.get('GROQ_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+
+extracted_data=load_pdf_files(data='data/')
+filter_data = filter_to_minimal_doc(extracted_data)
+text_chunks=text_splitter(filter_data)
+
+embeddings = download_embeddings_model()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=pinecone_api_key)
+
+
+
+index_name = "medical-chatbot"  # change if desired
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+
+index = pc.Index(index_name)
+
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings, 
+)
